a45e6af45e61ccdee17ff388d126fe16b6daebbb
Guifré Puig Ribas <guifreribas@gmail.com>
2024-11-19 11:00:22 +0100

    refactor worker.js to improve code formatting and integrate @mlc-ai/web-llm package for better consistency and functionality

--- src/utils/worker.js
+++ src/utils/worker.js
@@ -1,14 +1,15 @@
-import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm";
+// import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm";
+import { WebWorkerMLCEngineHandler } from "@mlc-ai/web-llm";
 
 const handler = new WebWorkerMLCEngineHandler();
 
 self.onmessage = (msg) => {
-	handler.onmessage(msg);
+  handler.onmessage(msg);
 };
 
 self.onerror = (e) => console.error("Worker error:", e.message);
 
 self.onmessage = (msg) => {
-	console.log("Message received in worker:", msg);
-	handler.onmessage(msg);
+  console.log("Message received in worker:", msg);
+  handler.onmessage(msg);
 };
